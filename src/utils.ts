162912f8f4e9bd66e5bf9156f856cb9684d5c9a9
Vladimir <sheremet.va@icloud.com>
2022-01-05 20:53:55 +0300

    fix: add more guesses for guessing content-type

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,13 @@ export function guessContentType(body: unknown) {
     }
   }
   if (body === null || body === undefined) return 'application/json';
-  if (body instanceof Blob) {
+  if (typeof FormData !== 'undefined' && body instanceof FormData) {
+    return 'multipart/form-data';
+  }
+  if (typeof ArrayBuffer !== 'undefined' && body instanceof ArrayBuffer) {
+    return 'application/octet-stream';
+  }
+  if (typeof Blob !== 'undefined' && body instanceof Blob) {
     return body.type;
   }
   if (typeof body === 'object') {
