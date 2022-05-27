c7b5d7750371508afeb636e72d1a8215f032877f
Vladimir Sheremet <sheremet.va@icloud.com>
2022-05-27 12:24:39 +0300

    fix: put globals only on globalThis

--- src/setup.ts
+++ src/setup.ts
@@ -3,24 +3,12 @@ import { prepareFetch } from './api.js';
 import { HeadersMock } from './headers.js';
 import { FormDataMock } from './formdata.js';
 
-// if happy dom
-if (
-  globalThis &&
-  globalThis.window &&
-  'fetch' in globalThis.window &&
-  globalThis.window.fetch
-) {
-  prepareFetch(globalThis.window);
-} else {
-  prepareFetch();
+prepareFetch();
+
+if (!('Headers' in globalThis)) {
+  globalThis.Headers = HeadersMock;
 }
 
-if (!('Headers' in global)) {
-  global.Headers = HeadersMock;
-  global.window.Headers = HeadersMock;
-}
-
-if (!('FormData' in global)) {
-  global.FormData = FormDataMock;
-  global.window.FormData = FormDataMock;
+if (!('FormData' in globalThis)) {
+  globalThis.FormData = FormDataMock;
 }
